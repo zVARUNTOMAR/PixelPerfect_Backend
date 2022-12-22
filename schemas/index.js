c3ab10ac1bb5a11fcb0191dd4fc63d451193a0e1
unknown <vrnkmr119@gmail.com>
2022-12-22 18:54:25 +0530

    inital commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from './user'
+import pin from './pin'
+import save from './save'
+import postedBy from './postedBy'
+import comment from './comment'
+export const schemaTypes = [user, pin, save, postedBy, comment]
